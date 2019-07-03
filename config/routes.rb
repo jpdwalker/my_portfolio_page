a073d6b5b31890113314bd0ca40c5e70c6a24841
Joe Walker <jpdwalker@gmail.com>
2019-07-03 22:41:07 +0100

    added projects resources and controller views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+  get 'projects/show'
   get 'articles/index'
   get 'articles/show'
   get "/", to: "homepage#home", as: "root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
+  resources :projects
 end
