2fd856b2a4828b6d86d4e49cfade8a6f6d9c2cb5
Joe Walker <jpdwalker@gmail.com>
2019-07-08 19:24:07 +0100

    added crud to articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   get 'pages/contact'
   get 'projects/index'
   get 'projects/show'
-  get 'articles/index'
-  get 'articles/show'
   get "/", to: "homepage#home", as: "root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
