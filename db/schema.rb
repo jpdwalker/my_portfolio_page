2658f36d08f567255bb3fc4b46973081f2bae9e4
Joe Walker <jpdwalker@gmail.com>
2019-07-10 13:39:07 +0100

    setup devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_102553) do
+ActiveRecord::Schema.define(version: 2019_07_10_123444) do
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -40,6 +40,18 @@ ActiveRecord::Schema.define(version: 2019_07_10_102553) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.index ["slug"], name: "index_projects_on_slug", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
